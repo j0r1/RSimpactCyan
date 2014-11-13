3d38797dcb3e397fe81ada2b468270f6aa9485eb
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2014-11-13 15:53:25 +0100

    Script to build binary package

---
+++ buildpkg.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import os
+import sys
+import subprocess
+import platform
+import shutil
+
+def getPackageFileName(logFileName):
+    with open(logFileName, "rt") as f:
+        l = f.readline()
+        while l:
+            l = l.strip()
+            if l.startswith("packaged installation of"):
+                s = "' as '"
+                idx = l.find(s)
+                if idx >= 0:
+                    startPos = idx+len(s)
+                    endPos = l.find("'", startPos)
+                    if endPos >= startPos:
+                        return l[startPos:endPos]
+
+            l = f.readline()
+
+def main():
+    instDir = sys.argv[1]
+
+    os.mkdir(instDir)
+    curDir = os.getcwd()
+    os.chdir(instDir)
+    instDir = os.getcwd()
+    tmpLibDir = os.path.join(instDir, "tmpLibDir")
+    os.mkdir(tmpLibDir)
+    
+    Rexe = "R"
+    logFileName = "rlogfile"
+    with open(logFileName, "wt") as logfile:
+        subprocess.call([ Rexe, "CMD", "INSTALL", curDir, "--build", "-l", tmpLibDir ], stdout=logfile, stderr=logfile)
+
+    pkgFile = getPackageFileName(logFileName)
+    os.remove(logFileName)
+
+    print("Package name is %s" % pkgFile)
+
+    if platform.system() == "Windows": # Windows
+        print("TODO")
+    elif platform.system() == "Darwin": # OS X
+        os.makedirs("bin/macosx/mavericks/contrib/3.1/")
+        os.makedirs("bin/macosx/contrib/3.0/")
+        shutil.copy(pkgFile, "bin/macosx/mavericks/contrib/3.1/")
+        shutil.move(pkgFile, "bin/macosx/contrib/3.0/")
+        shutil.copy(os.path.join(curDir,"DESCRIPTION"), "bin/macosx/mavericks/contrib/3.1/PACKAGES")
+        shutil.copy(os.path.join(curDir,"DESCRIPTION"), "bin/macosx/contrib/3.0/PACKAGES")
+    else:
+        print("Unknown platform, not creating directory structure")
+
+    shutil.rmtree(tmpLibDir)
+    
+if __name__ == "__main__":
+    main()
